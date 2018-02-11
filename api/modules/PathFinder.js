383e3136695b5804df3cf953fe88f876bd360c7e
Oussama LightRay <SakasakyRay@Gmail.com>
2018-02-11 22:32:49 +0100

    catch empty result errors

--- api/modules/PathFinder.js
+++ api/modules/PathFinder.js
@@ -6,11 +6,15 @@ var getPath = function(params, callback){
     DatabaseManager.getDirection(params, (result)=>{
         var steps = []
         
-        result[0]._fields[0].forEach(station => {
-            steps.push({
-                name: (station.properties.name)? station.properties.name : "?"
-            })
-        });
+        try {
+            result[0]._fields[0].forEach(station => {
+                steps.push({
+                    name: (station.properties.name)? station.properties.name : "?"
+                })
+            });
+        } catch (err) {
+            steps = []
+        }
         callback(steps)
     })
 }
