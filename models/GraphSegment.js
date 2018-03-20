3f0fccb02b4dbe281fa73731ce8d1ed12d893b80
Oussama LightRay <SakasakyRay@Gmail.com>
2018-03-20 01:14:09 +0100

    updates Graph segment's model

---
+++ models/GraphSegment.js
@@ -0,0 +1,26 @@
+/** 
+ * 
+*/
+class GraphSegment {
+
+    /**
+     * @param {ID} srcStationID ID of the source station
+     * @param {ID} destStationID 
+     * @param {TransportType} type 
+     * @param {Number} distance in meters
+     * @param {Number} avgTime average time to get from src to dst (in minutes)
+     * @param {Bus} bus 
+     */
+    constructor(srcStationID, destStationID, type, distance, avgTime, bus = {})
+    {
+        this.srcStationID = srcStationID
+        this.destStationID = destStationID
+        this.type = type
+        this.distance = distance
+        this.avgTime = avgTime
+        if(bus)
+            this.bus = bus
+    }
+}
+
+module.exports = GraphSegment
