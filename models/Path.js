1a86e2d5d556e92dc0ee0d51c9070b117203c3b7
Oussama LightRay <SakasakyRay@Gmail.com>
2018-03-20 01:13:30 +0100

    adds Path (query result) model

--- models/Path.js
+++ models/Path.js
@@ -1,4 +1,11 @@
+
 class Path {
+
+    /**
+     * @param {Number} dist total Distance (in meters)
+     * @param {Number} price total Price (in DA)
+     * @param {Number} time totalTime (in minutes)
+     */
     constructor(dist = 0, price = 0, time = 0) {
         this.totalDistance = dist
         this.totalPrice = price
@@ -8,6 +15,27 @@ class Path {
 
     addStep(step) {
         this.steps.push(step)
+        this.totalDistance += step.dist
+        this.totalPrice += step.price
+        this.totalTime += step.time
+    }
+
+    /** 
+     * Creates a new path from current with merged steps
+     * @return {Path} minimized Path
+    */
+    minimizePath() {
+        minPath = new Path()
+        
+        for(var i = 0; i < steps.length-1; ++i) {
+            // TODO : 
+            // test if steps[i] and steps[i+1] can be merged
+                // merge
+            // else
+                // add step[i]
+        }
+
+        // add this.steps[steps.length-1] to minPath
     }
 }
 
