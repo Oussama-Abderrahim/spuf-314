ce345610d10bde763b7eb910efaeec8ed4c41686
Oussama LightRay <SakasakyRay@Gmail.com>
2018-03-27 08:26:08 +0100

    refactores controllers

---
+++ controllers/index.js
@@ -0,0 +1,35 @@
+bodyParser = require("body-parser");
+const express = require("express");
+
+var PathFinder = require("./modules/PathFinder")
+
+var router = express.Router();
+
+router.route('/').get((req, res) => {
+    res.json({message:"Welcome to our API, see *link* for documentation"})
+});
+
+router.use('/station', require('./stations'))
+
+router.route('/direction').get((req, res)=>{
+    //TODO : check params 
+    if(req.query.start && req.query.end){
+        var params = {
+            start: req.query.start,
+            end: req.query.end,
+            transport: {
+                bus: (req.query.bus)? true: false,
+                tram: (req.query.tram)? true: false,
+                walk: (req.query.walk)? true: false
+            }
+        }
+        PathFinder.getPath(params, (result)=>{
+                res.json(result)
+        });
+    } else {
+        res.json({})
+    }
+});
+
+
+module.exports = router;
