c1c5486a3a3ac88c23e0f9da3baf065577fbbcd7
Oussama LightRay <SakasakyRay@Gmail.com>
2018-04-28 13:40:37 +0100

    Adds map marker in Create Station to fill data Added validator

--- app/admin/src/components/CreateStationDialog.vue
+++ app/admin/src/components/CreateStationDialog.vue
@@ -19,14 +19,14 @@
               <!-- CHAMP NOM STATION -->
               <v-flex xs8>
                 <v-container title>Informations générales:</v-container>
-                <v-text-field label="Nom de la station" v-model="station.name"></v-text-field>
-                <v-text-field label="Adresse de la station" v-model="station.address"></v-text-field>
+                <v-text-field label="Nom de la station" v-model="station.name" required></v-text-field>
+                <v-text-field label="Adresse de la station" v-model="station.address" required></v-text-field>
                 <v-text-field name="input-1-3" label="Informations sur la station" class="coordonnees-info"></v-text-field>
                 <br>
 
                 <v-container title prepend-icon="place">Les coordonnées:</v-container>
-                <v-text-field name="input-1-3" label="Longitude" v-model="station.coordLon"></v-text-field>
-                <v-text-field name="input-1-3" label="Latitude" v-model="station.coordLat"></v-text-field>
+                <v-text-field name="input-1-3" label="Longitude" v-model="station.coordLon" required></v-text-field>
+                <v-text-field name="input-1-3" label="Latitude" v-model="station.coordLat" required></v-text-field>
 
                 <v-btn small fab dark color="black">
                   <v-icon dark>add</v-icon>
@@ -43,13 +43,13 @@
             <v-layout row wrap align-center>
               <!-- CHAMP MAP -->
               <v-flex xs12 class="map">
-                <google-map name="creatStation" class="google-map"></google-map>
+                <google-map name="creatStation" @makerDragged='updateFromMap' class="google-map"></google-map>
               </v-flex>
 
               <!-- CHAMP BUTTONS -->
               <v-flex xs12 align-center>
                 <v-container class="buttons" align-center>
-                  <v-btn type="submit">Ajouter</v-btn>
+                  <v-btn type="submit" :loading='loading'>Ajouter</v-btn>
                   <v-btn type="cancel">Annuler</v-btn>
                 </v-container>
               </v-flex>
@@ -69,84 +69,115 @@
 
 
 <script>
-  /*  eslint-disable */
-  import MapSection from '../components/MapSection'
+/*  eslint-disable */
+import MapSection from '../components/MapSection'
 
-  export default {
-    name: 'creatStation',
-    components: {
-      'google-map': MapSection
-    },
-    data() {
-      return {
-        dialogCreate: false,
-        station: {
-          name : '',
-          address: '',
-          coordLat: 0,
-          coordLon: 0
+export default {
+  name: 'creatStation',
+  components: {
+    'google-map': MapSection
+  },
+  data() {
+    return {
+      dialogCreate: false,
+      loading: false,
+      station: {
+        name: '',
+        address: '',
+        coordLat: '',
+        coordLon: ''
+      }
+    }
+  },
+  mounted() {
+    this.$station = this.$resource(
+      'station{/id}',
+      {},
+      {},
+      {
+        before: () => {
+          this.loading = true
+        },
+        after: () => {
+          this.loading = false
         }
       }
+    )
+  },
+  methods: {
+    clear() {
+      this.station.name = ''
+      this.station.address = ''
+      this.station.coordLat = ''
+      this.station.coordLon = ''
     },
-    mounted() {
-      this.$station = this.$resource('station{/id}', {}, {}, {
-        before: () => {},
-        after: () => {}
-      })
+    updateFromMap(position) {
+      this.station.address = position.formattedAddress
+      this.station.coordLat = position.lat()
+      this.station.coordLon = position.lng()
     },
-    methods: {
-      submit() {
-        this.$station.save({}, {...this.station}).then(response => {
-          console.log(response)
-        }, error => {
-          console.log(err)
-        })
+    submit() {
+      if (this.station.name && this.station.coordLat && this.station.coordLon) {
+        confirm('Etes vous sûr de vouloir ajouter cette station ?') &&
+          this.$station.save({}, { ...this.station }).then(
+            response => {
+              alert('Station added successfully')
+              this.clear()
+            },
+            error => {
+              console.log(err)
+              alert('An error occured')
+            }
+          )
+      } else {
+        alert("Please fill the required fields")
       }
     }
   }
+}
 </script>
 
 <style lang="scss" scoped>
-  #create-station {
+#create-station {
+  position: relative;
+  overflow: hidden;
+  margin: auto;
+  width: 80%;
+  max-width: 100%;
+  display: flex;
+  color: black;
+}
+
+.push_down {
+  margin-bottom: 30px;
+}
+
+.push_up {
+  margin-top: 30px;
+}
+
+.map {
+  overflow: hidden;
+  position: relative;
+  padding: 0;
+  margin: 20px;
+  width: 200px;
+  height: 500px;
+  border: 1px solid black;
+  margin-top: 60px;
+
+  .google-map {
     position: relative;
-    overflow: hidden;
-    margin: auto;
-    width: 80%;
-    max-width: 100%;
-    display: flex;
-    color: black;
+    width: 100%;
+    height: 100%;
   }
+}
 
-  .push_down {
-    margin-bottom: 30px;
-  }
+.buttons {
+  text-align: center;
+}
 
-  .push_up {
-    margin-top: 30px;
-  }
-
-  .map {
-    overflow: hidden;
-    position: relative;
-    padding: 0;
-    margin: 20px;
-    width: 200px;
-    height: 500px;
-    border: 1px solid black;
-    margin-top: 60px;
-
-    .google-map {
-      position: relative;
-      width: 100%;
-      height: 100%;
-    }
-  }
-
-  .buttons {
-    text-align: center;
-  }
-
-  .coordonnees-info {
-    margin-top: 0px;
-  }
+.coordonnees-info {
+  margin-top: 0px;
+}
 </style>
--- app/admin/src/components/MapSection.vue
+++ app/admin/src/components/MapSection.vue
@@ -8,24 +8,78 @@
 export default {
   name: 'google-map',
   props: ['name'],
-  data: function () {
+  data: function() {
     return {
-      mapName: this.name + "-map",
+      mapName: this.name + '-map',
+      marker: null,
+      geoCoder: null,
+      infowindow: null,
+      map: null
     }
   },
-  mounted: function () {
+  mounted: function() {
+    const intialCoord = { lat: 35.69111, lng: -0.64167 }
+
     const element = document.getElementById(this.mapName)
     const options = {
       zoom: 14,
-      center: new google.maps.LatLng(35.69111, -0.64167)
+      center: intialCoord
     }
-    const map = new google.maps.Map(element, options)
+    this.map = new google.maps.Map(element, options)
+    this.geoCoder = new google.maps.Geocoder()
+    this.infowindow = new google.maps.InfoWindow()
+
+    // Create a marker and set its position.
+    this.marker = new google.maps.Marker({
+      map: this.map,
+      draggable: true,
+      position: intialCoord,
+      title: 'Marquer la station'
+    })
+
+    this.map.addListener('dblclick', e => {
+      this.placeMarkerAndPanTo(e.latLng, this.map)
+    })
+
+    this.marker.addListener('dragend', () => {
+      this.map.setZoom(this.map.getZoom() + 1)
+      this.map.setCenter(this.marker.getPosition())
+      this.geocodeLatLng(
+        this.map,
+        this.geoCoder,
+        this.marker.getPosition(),
+        this.infowindow
+      )
+    })
+
+  },
+  methods: {
+    placeMarkerAndPanTo(latLng, map) {
+      this.marker.setPosition(latLng)
+      map.panTo(latLng)
+    },
+    geocodeLatLng(map, geoCoder, latLng, infowindow) {
+      geoCoder.geocode({ location: latLng }, (results, status) => {
+        if (status == 'OK') {
+          infowindow.setContent(results[0].formatted_address)
+          infowindow.open(map, this.marker)
+          // Emit to parent
+          this.$emit('makerDragged', {
+            formattedAddress: results[0].formatted_address,
+            lat: latLng.lat,
+            lng: latLng.lng
+          })
+        } else {
+          alert('Geocoder failed due to: ' + status)
+        }
+      })
+    }
   }
 }
 </script>
 
 <style scoped>
 .google-map {
-    background: gray;
+  background: gray;
 }
 </style>
