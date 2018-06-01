4244a68d77b99a9df7026888a9afafec6fb5a4bb
Oussama LightRay <SakasakyRay@Gmail.com>
2018-06-01 01:32:04 +0100

    pathfinder reads neo4j result in a streaming manner as records arrive

--- controllers/modules/PathFinder.js
+++ controllers/modules/PathFinder.js
@@ -1,40 +1,60 @@
-const Path = require("../../models/Path");
-const Step = require("../../models/Step");
-const TransportType = require("../../models/TransportType");
+const Path = require('../../models/Path')
+const Step = require('../../models/Step')
+const TransportType = require('../../models/TransportType')
 
 module.exports = function(dbSession) {
-  const Station = require("../../models/Station")(dbSession);
-  const GraphNode = require("../../models/graphModels/GraphNode")(dbSession);
+  const Station = require('../../models/Station')(dbSession)
+  const GraphNode = require('../../models/graphModels/GraphNode')(dbSession)
 
+  const getPathFromRecord = record => {
+    let path = []
+    try {
+      path = _makePath(record)
+      path = path.minimize()
+      return path
+    } catch (err) {
+      console.error(err)
+      return []
+    }
+  }
   /**
    * @param {*} db_result a record from the database results
    */
   const _makePath = function(db_result) {
-    let segments = db_result.get("segments");
-    let startNodes = db_result.get("startNodes");
-    let endNodes = db_result.get("endNodes");
+    let segments = db_result.get('segments')
+    let startNodes = db_result.get('startNodes')
+    let endNodes = db_result.get('endNodes')
 
-    let path = new Path();
+    let path = new Path()
 
     for (let i = 0; i < segments.length; i++) {
-      let sourceStation = Station.convertNodeToStation(new GraphNode(startNodes[i]));
-      let destStation = Station.convertNodeToStation(new GraphNode(endNodes[i]));
+      let sourceStation = Station.convertNodeToStation(
+        new GraphNode(startNodes[i])
+      )
+      let destStation = Station.convertNodeToStation(new GraphNode(endNodes[i]))
 
-      
-      let type = TransportType.getTransportType(segments[i].type).label;
-      
-      let price = _getPrice(type);
-      let dist = segments[i].properties.distance.toNumber();
-      let time = segments[i].properties.avgTime.toNumber();
+      let type = TransportType.getTransportType(segments[i].type).label
 
-      let name = segments[i].properties.bus ? segments[i].properties.bus : "X";
+      let price = _getPrice(type)
+      let dist = segments[i].properties.distance.toNumber()
+      let time = segments[i].properties.avgTime.toNumber()
 
-      let step = new Step(sourceStation, destStation, price, dist, time, type, name);
+      let name = segments[i].properties.bus ? segments[i].properties.bus : 'X'
 
-      path.addStep(step);
+      let step = new Step(
+        sourceStation,
+        destStation,
+        price,
+        dist,
+        time,
+        type,
+        name
+      )
+
+      path.addStep(step)
     }
-    return path;
-  };
+    return path
+  }
 
   /**
    * Returns a predefined Default price for each type
@@ -44,15 +64,15 @@ module.exports = function(dbSession) {
   const _getPrice = function(type) {
     switch (type) {
       case TransportType.Bus.label:
-        return 20;
-        break;
+        return 20
+        break
       case TransportType.Tramway.label:
-        return 40;
-        break;
+        return 40
+        break
       default:
-        return 0;
+        return 0
     }
-  };
+  }
 
   /**
    * Computes the path from point A to B given in params
@@ -66,27 +86,8 @@ module.exports = function(dbSession) {
    * @param {Boolean} params.transport.walk true if to include walking in path
    */
   const getPath = function(params) {
-    return new Promise((resolve, reject) => {
-      computeDirection(params)
-        .then(db_results => {
-          let paths = [];
-
-          db_results.forEach((db_result, i) => {
-            let path = [];
-            try {
-              path = _makePath(db_result);
-              path = path.minimize();
-            } catch (err) {
-              console.log(err);   /// TODO : Add a logger
-              path = [];
-            }
-            paths.push(path);
-          });
-          resolve(paths);
-        })
-        .catch(err => reject(err));
-    });
-  };
+    return computeDirection(params)
+  }
 
   /**
    * Compute direction from Graphdatabase
@@ -100,15 +101,14 @@ module.exports = function(dbSession) {
    */
   const computeDirection = function(params) {
     return new Promise((resolve, reject) => {
-      let transports = [];
-      if (params.transport.bus) transports.push(TransportType.Bus.db_label);
-      if (params.transport.tram)
-        transports.push(TransportType.Tramway.db_label);
-      if (params.transport.walk) transports.push(TransportType.Walk.db_label);
+      let transports = []
+      if (params.transport.bus) transports.push(TransportType.Bus.db_label)
+      if (params.transport.tram) transports.push(TransportType.Tramway.db_label)
+      if (params.transport.walk) transports.push(TransportType.Walk.db_label)
 
-      dbSession
-        .run(
-          `MATCH p = AllShortestPaths((A:Station)-[*..20]->(B:Station))
+      let paths = []
+
+      const query = `MATCH p = AllShortestPaths((A:Station)-[*..20]->(B:Station))
             WHERE A.name = {startParam} AND B.name = {endParam}
             AND ALL(rel IN relationships(p) WHERE type(rel) IN {transportsParam})
             WITH p, RELATIONSHIPS(p) as segments
@@ -116,22 +116,30 @@ module.exports = function(dbSession) {
             EXTRACT (segment in segments| EndNode(segment)) AS endNodes,
             RELATIONSHIPS(p) as segments
             
-            return segments, startNodes, endNodes`,
-          {
-            startParam: params.start,
-            endParam: params.end,
-            transportsParam: transports
-          }
-        )
-        .then(result => {
-          dbSession.close();
-          resolve(result.records);
+            return segments, startNodes, endNodes`
+
+      dbSession
+        .run(query, {
+          startParam: params.start,
+          endParam: params.end,
+          transportsParam: transports
         })
-        .catch(err => reject(err));
-    });
-  };
+        .subscribe({
+          onNext: record => {
+            paths.push(getPathFromRecord(record))
+          },
+          onCompleted: () => {
+            dbSession.close()
+            resolve(paths)
+          },
+          onError: function(error) {
+            console.log(error)
+          }
+        })
+    })
+  }
 
   return {
     getPath
-  };
-};
+  }
+}
